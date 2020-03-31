ae12bb9370f9ddcbf50c9cc36438339e862f8fbe
Simon <simon@aparicio.org>
2020-04-01 00:29:41 +0200

    add cocoa pods - update .gitignore - Alamofire 5.0.5

---
+++ myHeroes/myHeroes/Source/ViewModel/HeroesListViewModel.swift
@@ -0,0 +1,22 @@
+//  HeroesListViewModel.swift
+//  myHeroes
+//
+//  Created by Simón Aparicio on 31/03/2020.
+//  Copyright © 2020 iPon.es. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class HeroesListViewModel {
+    
+    init() {
+        
+    }
+    
+    func getCharacters() {
+        
+        print("retrieving characters list from Webservice..")
+
+    }
+}
