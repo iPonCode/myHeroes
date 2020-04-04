8e41fd002a2a1ba97269aea1733b66d743940a9c
Simon <simon@aparicio.org>
2020-04-05 01:32:57 +0200

    added available series to sorting options

--- myHeroes/myHeroes/Source/Model/SortingType.swift
+++ myHeroes/myHeroes/Source/Model/SortingType.swift
@@ -13,9 +13,10 @@ enum SortingType: Int, CaseIterable { // Needs to iterate over allcases using a 
     case byId = 1
     case byAvailableComics = 2
     case byAvailableEvents = 3
-    case watched = 4
-    case favourite = 5
-    case featured = 6
+    case byAvailableSeries = 4
+    case watched = 5
+    case favourite = 6
+    case featured = 7
 
     init(type: Int) {
         
@@ -29,10 +30,12 @@ enum SortingType: Int, CaseIterable { // Needs to iterate over allcases using a 
         case 3:
             self = .byAvailableEvents
         case 4:
-            self = .watched
+            self = .byAvailableSeries
         case 5:
-            self = .favourite
+            self = .watched
         case 6:
+            self = .favourite
+        case 7:
             self = .featured
         default:
             self = .byName
@@ -50,6 +53,8 @@ enum SortingType: Int, CaseIterable { // Needs to iterate over allcases using a 
             return "Comics (available)"
         case .byAvailableEvents:
             return "Events (available)"
+        case .byAvailableSeries:
+            return "Series (available)"
         case .watched:
             return "Checked as Watched"
         case .favourite:
@@ -70,6 +75,8 @@ enum SortingType: Int, CaseIterable { // Needs to iterate over allcases using a 
                 return { descOrder ? ($0.comics.available < $1.comics.available) : ($0.comics.available > $1.comics.available) }
             case .byAvailableEvents:
                 return { descOrder ? ($0.events?.count ?? 0 < $1.events?.count ?? 0) : ($0.events?.count ?? 0 > $1.events?.count ?? 0) }
+            case .byAvailableSeries:
+                return { descOrder ? ($0.series?.count ?? 0 < $1.series?.count ?? 0) : ($0.series?.count ?? 0 > $1.series?.count ?? 0) }
             case .watched:
                 return descOrder ? {$0.watched && !$1.watched} : {!$0.watched && $1.watched}
             case .favourite:
@@ -101,9 +108,9 @@ enum SortingOptionType: Int, CaseIterable {
         
         switch self {
         case .ascending:
-            return "in ascending order"
+            return "in Ascending order"
         case .descending:
-            return "in descending order"
+            return "in Descending order"
         }
     }
     
