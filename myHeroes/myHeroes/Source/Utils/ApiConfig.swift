bd24defa446ba5a9d0e671b979f9f62e011b918a
Simon <simon@aparicio.org>
2020-04-08 03:02:27 +0200

    methods to create a time stamp and and the hash checsum

--- myHeroes/myHeroes/Source/Utils/ApiConfig.swift
+++ myHeroes/myHeroes/Source/Utils/ApiConfig.swift
@@ -11,50 +11,39 @@ import Foundation
 
 struct ApiConfig {
 
-    static let baseUrl = "https://gateway.marvel.com:443/v1/public/characters"
-    static let privateKey = "8074772204a5fa9445ca96c81837f2b6d85b546b"
-    static let publicKey = "1de4fe3d3a6a89ba1e61bb34889865f1"
+    static private let baseUrl = "https://gateway.marvel.com:443/v1/public/characters"
+    static private let privateKey = "8074772204a5fa9445ca96c81837f2b6d85b546b"
+    static private let publicKey = "1de4fe3d3a6a89ba1e61bb34889865f1"
     
-    static let charactersWebSearchUrl = "https://www.marvel.com/characters"
+    static let publicCharactersWebSearchUrl = "https://www.marvel.com/characters"
 
-    // TODO: Declare headers for webservice here if needed
-    
-    // methods
+    // MARK: Methods to obtain the urls
     static func getCharactersListUrl() -> String {
-        
-        let timeStamp = Date().timeIntervalSince1970
-        let ts = String(format:"%.f", timeStamp)
-        let hashChecksum = String(format: "%.f%@%@",
-                                  timeStamp,
-                                  ApiConfig.privateKey,
-                                  ApiConfig.publicKey).md5()
-        return ApiConfig.baseUrl + "?ts=" + ts + "&apikey=" + ApiConfig.publicKey + "&hash=" + hashChecksum
+        let timeStamp = getTimeStamp()
+        return baseUrl + "?ts=" + timeStamp + "&apikey=" + publicKey + "&hash=" + getHashCecksum(ts: timeStamp)
     }
-
+    
     static func getDetailsUrl(_ id: Int) -> String {
-        
-        let timeStamp = Date().timeIntervalSince1970
-        let ts = String(format:"%.f", timeStamp)
-        let hashChecksum = String(format: "%.f%@%@",
-                                  timeStamp,
-                                  ApiConfig.privateKey,
-                                  ApiConfig.publicKey)
-            .md5()
-
-        return ApiConfig.baseUrl + "/" + String(id) + "?ts=" + ts + "&apikey=" +
-               ApiConfig.publicKey + "&hash=" + hashChecksum
+        let timeStamp = getTimeStamp()
+        return baseUrl + "/" + String(id) + "?ts=" + timeStamp + "&apikey=" + publicKey + "&hash=" + getHashCecksum(ts: timeStamp)
     }
 
     static func getComicsItemUrl(_ resourceURI: String) -> String {
-        
-        let timeStamp = Date().timeIntervalSince1970
-        let ts = String(format:"%.f", timeStamp)
-        let hashChecksum = String(format: "%.f%@%@",
-                                  timeStamp,
-                                  ApiConfig.privateKey,
-                                  ApiConfig.publicKey).md5()
-        return resourceURI + "?ts=" + ts + "&apikey=" + ApiConfig.publicKey + "&hash=" + hashChecksum
+        let timeStamp = getTimeStamp()
+        return resourceURI + "?ts=" + timeStamp + "&apikey=" + publicKey + "&hash=" + getHashCecksum(ts: timeStamp)
     }
+    
+    // MARK: Hash methods
+    static private func getHashCecksum(ts: String) -> String {
+        return String(format: "%@%@%@", ts, privateKey, publicKey).md5()
+    }
+    
+    static private func getTimeStamp() -> String {
+        // returns allways a different time stamp
+        return String(format:"%.f", Date().timeIntervalSince1970)
+    }
+    
+    // TODO: Declare headers for webservice here if needed
     
     private init() {}
 }
